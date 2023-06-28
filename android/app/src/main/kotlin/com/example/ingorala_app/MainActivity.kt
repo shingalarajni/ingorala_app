92eb405fdc752f882bf3eff0f26852cccaa26c54
shingalarajni <rajni.shingala@gmail.com>
2023-06-28 22:25:16 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ingorala_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ingorala_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("All Contact"),
+      ),
+    );
+  }
+}
